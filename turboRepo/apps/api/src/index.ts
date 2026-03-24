da1911e90c1b1d71d3e681db9a017ccb0d431760
naol86 <naolkasinet@gmail.com>
2026-03-24 12:32:20 +0300

    backend api apps added to the mono repo

---
+++ turboRepo/apps/api/src/index.ts
@@ -0,0 +1,4 @@
+import express from 'express';
+const app = express();
+app.get('/', (req, res) => res.json({ message: 'Hello World' }));
+app.listen(8080, () => console.log('API running on port 3001'));
